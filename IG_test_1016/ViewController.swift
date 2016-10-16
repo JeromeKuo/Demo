41e18f53116e8af9f4b1b1baba838876f4577b8b
JeromeKuo <jeromekuo@weddingpass.tw>
2016-10-16 22:29:13 +0800

    Initial Commit

---
+++ IG_test_1016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IG_test_1016
+//
+//  Created by Jerome Kuo on 2016/10/16.
+//  Copyright © 2016年 Jerome Kuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
